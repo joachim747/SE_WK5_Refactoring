91ef01bb0fbc51c11a2eb92ddbf82815ce7fe1bb
joachim747 <32508829+joachim747@users.noreply.github.com>
2018-05-14 06:17:01 +0200

    Replace Temp with Query: totalfrequentRenterpoints instead of freuquentRenterPoints

--- src/Main/Customer.java
+++ src/Main/Customer.java
@@ -18,19 +18,17 @@ class Customer {
     }
     
     public String statement() {
-        int frequentRenterPoints = 0;
         Enumeration enum_rentals = rentals.elements();	    
         StringBuilder result = new StringBuilder("Rental Record for " + this.getName() + "\n");
         result.append("\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n");
 
         while (enum_rentals.hasMoreElements()) {
             Rental each = (Rental) enum_rentals.nextElement();
-            frequentRenterPoints += each.getFrequentRenterPoints();
             result.append("\t").append(each.getMovie().getTitle()).append("\t").append("\t").append(each.getDaysRented()).append("\t").append(String.valueOf(thisAmount)).append("\n");
         }
         //add footer lines
         result.append("Amount owed is ").append(String.valueOf(getTotalCharge)).append("\n");
-        result.append("You earned ").append(String.valueOf(frequentRenterPoints)).append(" frequent renter points");
+        result.append("You earned ").append(String.valueOf(getTotalFrequentRenterPoints())).append(" frequent renter points");
         return result.toString();
     }
 
@@ -42,6 +40,17 @@ class Customer {
             result += each.getCharge();
         }
         return result;
+    }
+
+    private int getTotalFrequentRenterPoints() {
+        int result = 0;
+        Enumeration rentals = this.rentals.elements();
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            result += each.getFrequentRenterPoints();
+        }
+        return result;
+
     }
 }
     
