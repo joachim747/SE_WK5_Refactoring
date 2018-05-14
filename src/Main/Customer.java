575e25feeec0ac82da38aaac6e43232c5008ab77
joachim747 <32508829+joachim747@users.noreply.github.com>
2018-05-14 06:04:33 +0200

    Removed thisAmount within Customer

--- src/Main/Customer.java
+++ src/Main/Customer.java
@@ -25,18 +25,12 @@ class Customer {
         result.append("\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n");
 
         while (enum_rentals.hasMoreElements()) {
-            double thisAmount;
             Rental each = (Rental) enum_rentals.nextElement();
-            //determine amounts for each line
-            thisAmount = each.getCharge();
-            // add frequent renter points
             frequentRenterPoints++;
-            // add bonus for a two day new release rental
             if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
                 frequentRenterPoints++;
-            //show figures for this rental
             result.append("\t").append(each.getMovie().getTitle()).append("\t").append("\t").append(each.getDaysRented()).append("\t").append(String.valueOf(thisAmount)).append("\n");
-            totalAmount += thisAmount;
+            totalAmount += each.getCharge();
         }
         //add footer lines
         result.append("Amount owed is ").append(String.valueOf(totalAmount)).append("\n");
